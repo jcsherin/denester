1ed857e757837164616674b65e568292c45d4abd
jcsherin <jacob@protoship.io>
2025-01-19 15:27:31 +0530

    Fixes unused warnings

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ fn main() {
     let s1 = flat_schema();
 
     println!("Schema -> {}", s1.name());
+    // TODO: add a nested schema formatter
     for (index, field) in s1.fields().iter().enumerate() {
-        println!("\t[{}] {:?}", index, field);
+        println!(
+            "\t[{}] name: {} type: {:?} optional: {}",
+            index,
+            field.name(),
+            field.data_type(),
+            field.is_nullable()
+        );
     }
 }
 
@@ -14,8 +21,15 @@ fn flat_schema() -> Schema {
     let name = Field::new("name", DataType::String, false);
     let age = Field::new("age", DataType::Integer, false);
     let verified = Field::new("verified", DataType::Boolean, true);
+    let groups = Field::new("groups", DataType::List(Box::new(DataType::Integer)), true);
+    let account = Field::new(
+        "account",
+        DataType::Struct(vec![name, age, verified]),
+        false,
+    );
 
-    SchemaBuilder::new("user", vec![name, age])
-        .field(verified)
+    SchemaBuilder::new("user", vec![])
+        .field(account)
+        .field(groups)
         .build()
 }
