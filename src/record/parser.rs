efc5d74001ca0e5514be3c76bac25de856ac2c86
jcsherin <jacob@protoship.io>
2025-01-26 16:04:19 +0530

    style: format ParseError

--- src/record/parser.rs
+++ src/record/parser.rs
@@ -5,11 +5,34 @@ use crate::record::field_path::{FieldPath, PathMetadata};
 
 #[derive(Debug)]
 pub enum ParseError<'a> {
-    RequiredFieldIsMissing { field_path: FieldPath<'a> },
-    RequiredFieldIsNull { field_path: FieldPath<'a> },
-    DataTypeMismatch { field_path: FieldPath<'a>, expected: &'a DataType, found: &'a DataType },
-    PathNotFoundInSchema { expected: FieldPath<'a>, found: Vec<String> },
-    DefinitionLevelOutOfBounds { path_metadata: PathMetadata<'a>, found: u8 },
-    RepetitionLevelOutOfBounds { path_metadata: PathMetadata<'a>, found: u8 },
-    Other { message: Cow<'a, str>, field_path: FieldPath<'a> },
+    RequiredFieldIsMissing {
+        field_path: FieldPath<'a>,
+    },
+    RequiredFieldIsNull {
+        field_path: FieldPath<'a>,
+    },
+    DataTypeMismatch {
+        field_path: FieldPath<'a>,
+        expected: &'a DataType,
+        found: &'a DataType,
+    },
+    PathNotFoundInSchema {
+        expected: FieldPath<'a>,
+        found: Vec<String>,
+    },
+    DefinitionLevelOutOfBounds {
+        path_metadata: PathMetadata<'a>,
+        found: u8,
+    },
+    RepetitionLevelOutOfBounds {
+        path_metadata: PathMetadata<'a>,
+        found: u8,
+    },
+    Other {
+        message: Cow<'a, str>,
+        field_path: FieldPath<'a>,
+        source: Option<Box<dyn Error + 'static>>,
+    },
+}
+
 }
