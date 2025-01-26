f57d84d5f309f26ec8df507070e138e809dc94a3
jcsherin <jacob@protoship.io>
2025-01-26 15:29:53 +0530

    feat: add parse error categories for parsing nested values

--- src/record/mod.rs
+++ src/record/mod.rs
@@ -2,6 +2,7 @@ mod field;
 mod field_path;
 mod schema;
 mod value;
+mod parser;
 
 pub use field::{DataType, Field};
 pub use schema::{Schema, SchemaBuilder};
---
+++ src/record/parser.rs
@@ -0,0 +1,15 @@
+use std::borrow::Cow;
+use std::error::Error;
+use crate::record::DataType;
+use crate::record::field_path::{FieldPath, PathMetadata};
+
+#[derive(Debug)]
+pub enum ParseError<'a> {
+    RequiredFieldIsMissing { field_path: FieldPath<'a> },
+    RequiredFieldIsNull { field_path: FieldPath<'a> },
+    DataTypeMismatch { field_path: FieldPath<'a>, expected: &'a DataType, found: &'a DataType },
+    PathNotFoundInSchema { expected: FieldPath<'a>, found: Vec<String> },
+    DefinitionLevelOutOfBounds { path_metadata: PathMetadata<'a>, found: u8 },
+    RepetitionLevelOutOfBounds { path_metadata: PathMetadata<'a>, found: u8 },
+    Other { message: Cow<'a, str>, field_path: FieldPath<'a> },
+}
