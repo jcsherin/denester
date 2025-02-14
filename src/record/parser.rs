953248c79d10804037c0e2d8ff8616ba39a3d8f1
jcsherin <jacob@protoship.io>
2025-02-14 17:46:40 +0530

    refactor: remove unused ParseError variants

--- src/record/parser.rs
+++ src/record/parser.rs
@@ -7,37 +7,11 @@ use std::fmt::{write, Display, Formatter};
 
 #[derive(Debug)]
 pub enum ParseError<'a> {
-    UnknownField {
-        path: String,
-        field_name: String,
-    },
     RequiredFieldIsMissing {
         field_path: FieldPath<'a>,
     },
     RequiredFieldIsNull {
         field_path: FieldPath<'a>,
-    },
-    DataTypeMismatch {
-        field_path: FieldPath<'a>,
-        expected: &'a DataType,
-        found: &'a DataType,
-    },
-    PathNotFoundInSchema {
-        expected: FieldPath<'a>,
-        found: Vec<String>,
-    },
-    DefinitionLevelOutOfBounds {
-        path_metadata: PathMetadata<'a>,
-        found: u8,
-    },
-    RepetitionLevelOutOfBounds {
-        path_metadata: PathMetadata<'a>,
-        found: u8,
-    },
-    Other {
-        message: Cow<'a, str>,
-        field_path: FieldPath<'a>,
-        source: Option<Box<dyn Error + 'static>>,
     },
     UnexpectedTopLevelValue {
         value: &'a Value,
@@ -54,64 +28,11 @@ pub enum ParseError<'a> {
 impl<'a> Display for ParseError<'a> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            ParseError::UnknownField { path, field_name } => {
-                write!(f, "Field name: {} not found in path: {}", field_name, path)
-            }
             ParseError::RequiredFieldIsMissing { field_path } => {
                 write!(f, "Required field is missing: {}", field_path)
             }
             ParseError::RequiredFieldIsNull { field_path } => {
                 write!(f, "Required field is null: {}", field_path)
-            }
-            ParseError::DataTypeMismatch {
-                field_path,
-                expected,
-                found,
-            } => {
-                write!(
-                    f,
-                    "Data type mismatch: expected {}, found {} at {}",
-                    expected, found, field_path
-                )
-            }
-            ParseError::PathNotFoundInSchema { expected, found } => {
-                write!(
-                    f,
-                    "Path not found: {}, but expected {}",
-                    found.join("."),
-                    expected
-                )
-            }
-            ParseError::DefinitionLevelOutOfBounds {
-                path_metadata,
-                found,
-            } => {
-                write!(
-                    f,
-                    "Definition level is out of bound: expected {}, found definition level: {}",
-                    path_metadata, found
-                )
-            }
-            ParseError::RepetitionLevelOutOfBounds {
-                path_metadata,
-                found,
-            } => {
-                write!(
-                    f,
-                    "Repetition level is out of bound: expected {}, found repetition level: {}",
-                    path_metadata, found
-                )
-            }
-            ParseError::Other {
-                message,
-                field_path,
-                source,
-            } => {
-                if let Some(err) = source {
-                    write!(f, "{} at {}. source: {}", message, field_path, err)
-                } else {
-                    write!(f, "ParseError: {} at {}", message, field_path)
-                }
             }
             ParseError::UnexpectedTopLevelValue { value } => {
                 write!(f, "Expected top-level struct, found: {}", value)
@@ -131,15 +52,6 @@ impl<'a> Display for ParseError<'a> {
                         .join(",")
                 )
             }
-        }
-    }
-}
-
-impl<'a> Error for ParseError<'a> {
-    fn source(&self) -> Option<&(dyn Error + 'static)> {
-        match self {
-            ParseError::Other { source, .. } => source.as_deref(),
-            _ => None,
         }
     }
 }
