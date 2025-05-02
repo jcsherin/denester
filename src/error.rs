f3b9e9f11d386832831b4695347a5961aeb0f925
jcsherin <jacob@protoship.io>
2025-05-02 06:19:57 +0530

    feat: Implement Display trait for DenesterError

--- src/error.rs
+++ src/error.rs
@@ -54,8 +54,77 @@ pub enum DenesterError {
 }
 
 impl Display for DenesterError {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DenesterError::NullValueInRequiredField {
+                field_name,
+                type_label,
+                path_str,
+            } => {
+                write!(
+                    f,
+                    "Expected datatype: '{}' value but found NULL instead, field: {} path: {}",
+                    type_label, field_name, path_str
+                )
+            }
+            DenesterError::MissingOneOrMoreRequiredValues {
+                missing_field_names,
+                path_str,
+            } => {
+                write!(
+                    f,
+                    "Struct value is missing ({}) field name at path: {}",
+                    missing_field_names.join(","),
+                    path_str
+                )
+            }
+            DenesterError::InputValueMustBeAStruct => {
+                write!(f, "Parser input is not a struct value.")
+            }
+            DenesterError::ListElementDoesNotMatchSchema {
+                field_name,
+                path_str,
+                index,
+            } => {
+                write!(
+                    f,
+                    "List element at index: {} in field: {} at path: {}\
+                        does not match the list element type defined in schema",
+                    index, field_name, path_str
+                )
+            }
+            DenesterError::ValueTypeDoesNotMatchSchema {
+                field_name,
+                path_str,
+                expected_type_label,
+            } => {
+                write!(
+                    f,
+                    "Value datatype does not match expected type: {} for field: {} at path: {}",
+                    expected_type_label, field_name, path_str
+                )
+            }
+            DenesterError::StructContainsDuplicateProperty {
+                dup_property_name,
+                path_str,
+            } => {
+                write!(
+                    f,
+                    "Struct contains duplicate property: {} at path: {}",
+                    dup_property_name, path_str
+                )
+            }
+            DenesterError::StructContainsUndefinedProperty {
+                undefined_property_name,
+                path_str,
+            } => {
+                write!(
+                    f,
+                    "Struct contains undefined property: {} at path: {}",
+                    undefined_property_name, path_str
+                )
+            }
+        }
     }
 }
 
